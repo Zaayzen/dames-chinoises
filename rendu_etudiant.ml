be97528527a8f67bcd225c6df30f6bab503df17c
Maat3o <mateo.guglac@gmail.com>
2024-11-12 22:11:43 +0100

    Add files via upload

--- rendu_etudiant.ml
+++ rendu_etudiant.ml
@@ -42,7 +42,7 @@ let est_dans_losange ((i, j, k) : case) dim =
   (k <= dim) && (k >= -dim) &&
   (i + j + k = 0);;
 
-(*fonction test*)
+(* fonctions tests de est_dans_losange *)
 let()=
   let case = (0, 0, 0) in
   if est_dans_losange case dim then
@@ -53,11 +53,15 @@ let()=
 
 let est_dans_etoile ((i, j, k) : case) dim =
   (* union des 3 losanges *)
+  (* 1ere facon de le faire
   ((j <= dim) && (j >= -dim) && (k <= dim) && (k >= -dim) && (i + j + k = 0)) ||
   ((i <= dim) && (i >= -dim) && (k <= dim) && (k >= -dim) && (i + j + k = 0)) ||
   ((i <= dim) && (i >= -dim) && (j <= dim) && (j >= -dim) && (i + j + k = 0));;
+  *)
+  (* amélioration de la fonction *)
+  est_dans_losange (i, j, k) dim || est_dans_losange (j, k, i) dim || est_dans_losange (k, i, j) dim;;
 
-(*fonction test*)
+(* fonctions tests de est_dans_etoile *)
 let()= 
   let case = (0, 0, 0) in
   if est_dans_etoile case dim then
@@ -71,7 +75,7 @@ let quelle_couleur (case : case) (configuration : configuration) : couleur =
   if est_dans_etoile case (dim) then Libre
   else Dehors;; 
 
-(*fonction test*)
+(* fonctions tests de quelle_couleur *)
 let()=
   let case = (0, 0, 0) in
   let configuration = ([], [ Vert; Jaune; Rouge; Noir; Bleu; Marron ]) in
@@ -85,7 +89,7 @@ let tourne_case m ((i, j, k): case) =
     else rotate_case (m - 1) (-k, -i, -j) 
       in rotate_case m (i, j, k);;
 
-(*fonction test*)
+(* fonctions tests de tourne_case *)
 let()=
   let case = (0, 0, 0) in
   let m = 1 in
@@ -96,7 +100,7 @@ let()=
 let translate ((c1, c2, c3): case) ((v1, v2, v3): vecteur) : case =
   (c1 + v1, c2 + v2, c3 + v3);;
 
-(*fonction test*)
+(* fonctions tests de translate *)
 let()=
   let case = (0, 0, 0) in
   let vecteur = (1, 1, 1) in
@@ -104,10 +108,10 @@ let()=
   Printf.printf "La case translatee est (%d, %d, %d)" i j k;;
 
 
-let diff_case (c1a, c2a, c3a) (c1b, c2b, c3b) : case =
+let diff_case ((c1a, c2a, c3a) : case) ((c1b, c2b, c3b) : case) : vecteur =
   (c1a - c1b, c2a - c2b, c3a - c3b);;
 
-(*fonction test*)
+(* fonctions tests de diff_case *)
 let()=
   let case1 = (0, 0, 0) in
   let case2 = (1, 1, 1) in
@@ -126,7 +130,7 @@ let sont_cases_voisines (case1 : case) (case2 : case) : bool =
     (c1a, c2a, c3a) = (c1b + 1, c2b - 1, c3b) then true
   else false;;
 
-(*fonction test*)
+(* fonctions tests de sont_cases_voisines *)
 let()= 
   let case1 = (0, 0, 0) in
   let case2 = (1, 0, -1) in
@@ -136,19 +140,18 @@ let()=
     Printf.printf "Les cases ne sont pas voisines";;
 
 
-let calcul_pivot (c1 : case) (c2 : case) : case option =
-  let (c1a, c1b, c1c) = c1 in
-  let (c2a, c2b, c2c) = c2 in
-  if (c1a = c2a && (c1b + c2b) mod 2 = 0 && (c1c + c2c) mod 2 = 0) then
-    Some (c1a, (c1b + c2b) / 2, (c1c + c2c) / 2)
-  else if (c1b = c2b && (c1a + c2a) mod 2 = 0 && (c1c + c2c) mod 2 = 0) then
-    Some ((c1a + c2a) / 2, c1b, (c1c + c2c) / 2)
-  else if (c1c = c2c && (c1a + c2a) mod 2 = 0 && (c1b + c2b) mod 2 = 0) then
-    Some ((c1a + c2a) / 2, (c1b + c2b) / 2, c1c)
-  else
-    None;;
+let calcul_pivot (case1 : case) (case2 : case) : case option =
+  let (c1a, c2a, c3a) = case1 in
+  let (c1b, c2b, c3b) = case2 in
+  if (c1a = c1b && (c2a + c2b) mod 2 = 0 && (c3a + c3b) mod 2 = 0) then
+    Some (c1a, (c2a + c2b) / 2, (c3a + c3b) / 2)
+  else if (c2a = c2b && (c1a + c1b) mod 2 = 0 && (c3a + c3b) mod 2 = 0) then
+    Some ((c1a + c1b) / 2, c2a, (c3a + c3b) / 2)
+  else if (c3a = c3b && (c1a + c1b) mod 2 = 0 && (c2a + c2b) mod 2 = 0) then
+    Some ((c1a + c1b) / 2, (c2a + c2b) / 2, c3a)
+  else None;;
 
-(*fonction test*)
+(* fonctions tests de calcul_pivot *)
 let()=
   let case1 = (0, 2, -2) in
   let case2 = (0, -2, 2) in
@@ -157,15 +160,15 @@ let()=
   | None -> Printf.printf "Pas de pivot\n";;
 
 
-let vec_et_dist (c1 : case) (c2 : case) : vecteur * int =
-  let (dx, dy, dz) = diff_case c1 c2 in
+let vec_et_dist (case1 : case) (case2 : case) : vecteur * int =
+  let (dx, dy, dz) = diff_case case1 case2 in
   let d = (abs dx + abs dy + abs dz) / 2 in
   let unit_vector = (dx / d, dy / d, dz / d) in
   (unit_vector, d);;
 
-(*fonction test*)
+(* fonctions tests de vec_et_dist *)
 let()=
-  let c1 = (0,2,-2) in
-  let c2 = (0,0,0) in
-  let ((i, j, k : vecteur), d) = vec_et_dist c1 c2 in
+  let case1 = (0,2,-2) in
+  let case2 = (0,0,0) in
+  let ((i, j, k : vecteur), d) = vec_et_dist case1 case2 in
   Printf.printf "Vecteur: (%d,%d,%d), Distance : %d\n" i j k d;;
