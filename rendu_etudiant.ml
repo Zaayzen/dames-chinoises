c8fad9dbd4dce6544e4f8e00dc611eebe11da657
Zaayzen <112573016+Zaayzen@users.noreply.github.com>
2024-11-12 12:50:47 +0100

    Add files via upload

--- rendu_etudiant.ml
+++ rendu_etudiant.ml
@@ -42,16 +42,36 @@ let est_dans_losange ((i, j, k) : case) dim =
   (k <= dim) && (k >= -dim) &&
   (i + j + k = 0);;
 
+let()=
+  let case = (0, 0, 0) in
+  if est_dans_losange case dim then
+    Printf.printf "La case est dans le losange"
+  else
+    Printf.printf "La case n'est pas dans le losange";;
+
 let est_dans_etoile ((i, j, k) : case) dim =
   (* union des 3 losanges *)
   ((j <= dim) && (j >= -dim) && (k <= dim) && (k >= -dim) && (i + j + k = 0)) ||
   ((i <= dim) && (i >= -dim) && (k <= dim) && (k >= -dim) && (i + j + k = 0)) ||
   ((i <= dim) && (i >= -dim) && (j <= dim) && (j >= -dim) && (i + j + k = 0));;
 
+let()= 
+  let case = (0, 0, 0) in
+  if est_dans_etoile case dim then
+    Printf.printf "La case est dans l'etoile"
+  else
+    Printf.printf "La case n'est pas dans l'etoile";;
+
 let quelle_couleur (case : case) (configuration : configuration) : couleur =
   let (cases, _) = configuration in
   if est_dans_etoile case (dim) then Libre
   else Dehors;; 
+
+let()=
+  let case = (0, 0, 0) in
+  let configuration = ([], [ Vert; Jaune; Rouge; Noir; Bleu; Marron ]) in
+  let couleur = quelle_couleur case configuration in
+  Printf.printf "La couleur de la case est %s" (string_of_couleur couleur);;
 
 let tourne_case m ((i, j, k): case) =
   let rec rotate_case m (i, j, k) =
@@ -59,11 +79,29 @@ let tourne_case m ((i, j, k): case) =
     else rotate_case (m - 1) (-k, -i, -j) 
       in rotate_case m (i, j, k);;
 
-let translate (c1, c2, c3) (v1, v2, v3) : case =
+let()=
+  let case = (0, 0, 0) in
+  let m = 1 in
+  let (i, j, k) = tourne_case m case in
+  Printf.printf "La case tournee est (%d, %d, %d)" i j k;;
+
+let translate ((c1, c2, c3): case) ((v1, v2, v3): vecteur) : case =
   (c1 + v1, c2 + v2, c3 + v3);;
+
+let()=
+  let case = (0, 0, 0) in
+  let vecteur = (1, 1, 1) in
+  let (i, j, k) = translate case vecteur in
+  Printf.printf "La case translatee est (%d, %d, %d)" i j k;;
 
 let diff_case (c1a, c2a, c3a) (c1b, c2b, c3b) : case =
   (c1a - c1b, c2a - c2b, c3a - c3b);;
+
+let()=
+  let case1 = (0, 0, 0) in
+  let case2 = (1, 1, 1) in
+  let (i, j, k) = diff_case case1 case2 in
+  Printf.printf "La difference des cases est (%d, %d, %d)" i j k;;
 
 let sont_cases_voisines (case1 : case) (case2 : case) : bool =
   let (c1a, c2a, c3a) = case1  in
@@ -103,8 +141,6 @@ let()=
   match calcul_pivot case1 case2 with
   | Some (c1, c2, c3) -> Printf.printf "Le pivot est (%d, %d, %d)\n" c1 c2 c3
   | None -> Printf.printf "Pas de pivot\n";;
-
-
 
 let vec_et_dist (c1 : case) (c2 : case) : vecteur * int =
   let (dx, dy, dz) = diff_case c1 c2 in
